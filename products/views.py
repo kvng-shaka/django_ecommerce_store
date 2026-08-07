f737c86554d96da525033abf2e2b431848ccc39f
kvng-shaka <funsho.adeosun01@gmail.com>
2026-08-07 19:36:45 +0100

    Add product search filters and pagination

--- products/views.py
+++ products/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import get_object_or_404, render
-
-from products.models import Product
+from django.db.models import Q
+from products.models import Product, Category
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def product_list(request):
+    search = request.GET.get('search', '').strip()
+    category = request.GET.get('category', '').strip()
+    min_price = request.GET.get('min_price', '').strip()
+    max_price = request.GET.get('max_price', '').strip()
+
     products = Product.objects.filter(is_available=True)
-    return render(request, 'products/product_list.html', {'products': products})
+    categories = Category.objects.all()
+
+    # Search filter
+    if search:
+        products = products.filter(Q(name__icontains=search) | Q(description__icontains=search))
+
+    # Category filter
+    if category:
+        products = products.filter(category__name=category)
+
+    # Minimum price
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    # Maximum price
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    paginator = Paginator(products, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'products/product_list.html', {'products': page_obj, 'page_obj': page_obj, 'search':search, 'category': category, 'categories': categories, 'min_price': min_price, 'max_price': max_price,})
 
 
 def product_detail(request, pk):
